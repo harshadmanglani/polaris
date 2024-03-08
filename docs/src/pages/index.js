98786740be5d059b05e68482f339eaaff4d10778
Harshad Manglani <harshad.gm@gmail.com>
2024-03-08 09:39:23 +0530

    Updated imports in index.js

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,8 +1,6 @@
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
